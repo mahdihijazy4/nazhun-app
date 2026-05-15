950295ea1f8913b3cec32c25e200431b2e3958ef
Mahdi Hijazy <mahdihijazy4@users.noreply.github.com>
2026-05-15 19:56:46 +0300

    Fix network and location permissions

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.nazhun.app',
   appName: 'Nazhun',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https',
+    cleartext: true
+  }
 };
 
 export default config;
